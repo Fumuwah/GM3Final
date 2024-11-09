0d0853ea7a293cb3d38f2a8319d8befef91ee649
Dandre Collera <dandrecollera@gmail.com>
2024-11-10 00:02:05 +0800

    email function

--- common/mailer.php
+++ common/mailer.php
@@ -1,15 +1,14 @@
 <?php
-
-require '../phpmailer/src/Exception.php';
-require '../phpmailer/src/PHPMailer.php';
-require '../phpmailer/src/SMTP.php';
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+require './phpmailer/src/Exception.php';
+require './phpmailer/src/PHPMailer.php';
+require './phpmailer/src/SMTP.php';
 
 function smtp_mailer($to, $subject, $msg)
 {
+    $log_file = './mail_log.txt';  // Define log file location
     $mail = new PHPMailer();
     $mail->IsSMTP();
     $mail->SMTPAuth = true;
@@ -24,11 +23,21 @@ function smtp_mailer($to, $subject, $msg)
         )
     );
     $mail->IsHTML(true);
-    $mail->Username = "mddrmo.morong@gmail.com";
-    $mail->Password = "unjirgextfwgrhuq";
-    $mail->SetFrom("mddrmo.morong@gmail.com");
+    $mail->Username = "gm3buildershris@gmail.com";
+    $mail->Password = "jadv iloo ufiy tjyi";
+    $mail->SetFrom("gm3buildershris@gmail.com");
     $mail->Subject = $subject;
     $mail->Body = $msg;
     $mail->AddAddress($to);
-    $mail->Send();
+    
+    if ($mail->Send()) {
+        // Log success message
+        file_put_contents($log_file, "Mail sent successfully to $to\n", FILE_APPEND);
+        return true;
+    } else {
+        
+        // Log error message
+        file_put_contents($log_file, "Failed to send mail: " . $mail->ErrorInfo . "\n", FILE_APPEND);
+        return false;
+    }
 }
---
+++ test_mail.php
@@ -0,0 +1,5 @@
+<?php
+
+include './common/mailer.php';
+
+smtp_mailer('dandrecollera@gmail.com', 'Email from the system', 'this is a sample message');
